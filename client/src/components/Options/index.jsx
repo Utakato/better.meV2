770818906246bffe8fe9bde44bae6f12feaca4aa
Utakato <copyresearch98@gmail.com>
2021-12-10 12:42:43 +0200

    Add Options component / style

---
+++ client/src/components/Options/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+const Options = () => {
+    return (
+        <div className="options">
+            <div>X items left</div>
+            <div>Clear Completed</div>
+        </div>
+    )
+}
+
+export default Options
