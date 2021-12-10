37f1634fb35f52d73d2d5556cde9493e80a2f052
Utakato <copyresearch98@gmail.com>
2021-12-10 12:33:35 +0200

    Add Main and Header to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
 import TaskList from "./components/TaskList"
+
 function App() {
   return (
     <div className="App">
-    <TaskList />  
+      <Header />
+      <Main />
     </div>
   );
 }
