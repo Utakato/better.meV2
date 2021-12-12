5a564d0e80085f9128ec895545b4ffc52549778a
Utakato <copyresearch98@gmail.com>
2021-12-12 17:28:39 +0200

    Change items left to show not completed items

--- client/src/components/Options/index.jsx
+++ client/src/components/Options/index.jsx
@@ -4,14 +4,14 @@ import * as actions from "../../actions/taskActions"
 const Options = () => {
     const dispatch = useDispatch()
     const taskStore = useSelector(state => state.task)
-    const tasksLeft = taskStore.length
+    const tasksLeft = taskStore.filter(task => !task.completed)
     const tasksCompleted = taskStore.filter(task => task.completed && task )
     const handleClear = () => {
         dispatch(actions.removeTasks(tasksCompleted))
     }
     return (
         <div className="options">
-            <div>{tasksLeft} items left</div>
+            <div>{tasksLeft.length} items left</div>
             <div className ="clickable" onClick={handleClear}>Clear Completed</div>
         </div>
     )
