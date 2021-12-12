9831da222b97bb42c67300797a0a08c35e50566b
Utakato <copyresearch98@gmail.com>
2021-12-12 16:54:18 +0200

    Change TaskList to return tasks by order

--- client/src/components/TaskList/index.jsx
+++ client/src/components/TaskList/index.jsx
@@ -6,10 +6,21 @@ import { useSelector } from "react-redux"
 
 const TaskList = () => {
     const taskList = useSelector(state => state.task)
+    const order = useSelector(state => state.order)
     return (
         <div className="task-list">
             {taskList.map(task => {
-                return <Task name={task.content} completed={task.completed}/>
+                switch (order) {
+                    case "all":
+                        return <Task name={task.content} completed={task.completed}/>                        
+                    case "active": 
+                        return !task.completed && <Task name={task.content} completed={task.completed}/>
+                    case "completed": 
+                        return task.completed && <Task name={task.content} completed={task.completed}/>                
+                    
+                    default:
+                        break;
+                }
             })
             }
             <Options />
