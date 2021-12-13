fb98ebea5949d229108732520557427ce7d1eafb
Utakato <copyresearch98@gmail.com>
2021-12-13 15:35:08 +0200

    Fix checkbox completed

--- client/src/components/Task/index.jsx
+++ client/src/components/Task/index.jsx
@@ -1,29 +1,28 @@
 import "./style.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useDispatch } from "react-redux"
 import * as actions from "../../actions/taskActions"
 const Task = ({name, completed}) => {
     const [isShown, setIsShown] = useState(false)
+    const [checked, setChecked] = useState(completed)
     const dispatch = useDispatch()
-
     const handleDeleteClick = () => {
         dispatch(actions.removeTask(name)) // prob change name to id;
     }
     const handleChange = e => {
+        setChecked(!checked)
         let task = {
             content: name,
             completed: e.target.checked
         }
         dispatch(actions.updateTask(task))
-
     }
-
     return (
         <div className={completed ? "card completed": "card"}
              onMouseEnter={() => setIsShown(true)}
              onMouseLeave={() => setIsShown(false)}
              >
-            <input type="checkbox" onChange={handleChange}/>
+            <input type="checkbox" onChange={handleChange} checked={checked}/>
             <div>{name}</div>
             {isShown ? <i class="fas fa-times" onClick={handleDeleteClick}></i> : null}
         </div>
